8923488dd71a6de0aaead112d11d29f6d3aa154a
kordya20 <kordya20@yahoo.com>
2018-09-13 05:44:20 -0700

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task.Utility;
+
+namespace Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        Validation validation = new Validation();       // GET: Home
+        public ActionResult Index(string input)
+        {
+            input = "12345678";
+
+
+
+            if (!validation.MaxLength(7, input))
+            {
+                ViewBag.Err = "تعداد کارکتر ها را بیش از حد مجاز وارد کرده اید";
+            }
+
+
+
+
+
+            return View();
+        }
+    }
+}
